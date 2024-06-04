67d5ce9ac2f563794384bbefb283766336a3ae53
shivamdubey-07 <shivam.12016042@lpu.in>
2024-06-04 19:35:06 +0530

    auth.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const cors=require("cors")
 app.use(cookieParser());
 
 const corsOptions = {
-  origin:"http://localhost:3000",
+  origin:"*",
   credentials: true,
 };
 app.use(cors(corsOptions));
